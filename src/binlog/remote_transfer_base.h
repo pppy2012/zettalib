3c91c4daf152bbcdce7412dcfa95267e4c11fe10
snowao <snow@zettadb.com>
2022-02-10 10:02:01 +0800

    Add License related info

--- src/binlog/remote_transfer_base.h
+++ src/binlog/remote_transfer_base.h
@@ -1,3 +1,9 @@
+/*
+  Copyright (c) 2019-2021 ZettaDB inc. All rights reserved.
+
+  This source code is licensed under Apache 2.0 License,
+  combined with Common Clause Condition 1.0, as detailed in the NOTICE file.
+*/
 #ifndef _REMOTE_TRANSFER_BASE_H_
 #define _REMOTE_TRANSFER_BASE_H_
 
--- src/error/errorcup.h
+++ src/error/errorcup.h
@@ -1,3 +1,9 @@
+/*
+  Copyright (c) 2019-2021 ZettaDB inc. All rights reserved.
+
+  This source code is licensed under Apache 2.0 License,
+  combined with Common Clause Condition 1.0, as detailed in the NOTICE file.
+*/
 #ifndef _ERR_CUP_H_
 #define _ERR_CUP_H_
 
